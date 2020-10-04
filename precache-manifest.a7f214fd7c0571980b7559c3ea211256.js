84fe121dd3f262f468291048e164e063f9357c0b
Saurabh Singh <saur2702@gmail.com>
2020-10-04 19:52:52 +0530

    Updates

---
+++ precache-manifest.a7f214fd7c0571980b7559c3ea211256.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f823d694344079edb022d04074263db",
+    "url": "/covid19-tracker/index.html"
+  },
+  {
+    "revision": "24eeea7ed57290eb8d60",
+    "url": "/covid19-tracker/static/css/main.9fec9ef8.chunk.css"
+  },
+  {
+    "revision": "5c041fcabf27e252914e",
+    "url": "/covid19-tracker/static/js/2.a0aff664.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/covid19-tracker/static/js/2.a0aff664.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24eeea7ed57290eb8d60",
+    "url": "/covid19-tracker/static/js/main.66ddc3d0.chunk.js"
+  },
+  {
+    "revision": "6b444aef87d753a0bf04",
+    "url": "/covid19-tracker/static/js/runtime-main.815270a9.js"
+  }
+]);
