8c723c87ba5be9a12892aab85b7ff4e0e06bf3ce
Saurabh Singh <saur2702@gmail.com>
2020-10-04 20:16:01 +0530

    adding PUBLIC_URL to BrowserRouter

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
         <Routes />
       </BrowserRouter>
     </div>
